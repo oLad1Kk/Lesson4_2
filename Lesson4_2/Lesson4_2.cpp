8e00f22c11a60a14f14b7d02490d920951949b44
Vlad <vasvladikus@gmail.com>
2024-09-26 17:39:48 +0200

    MovieDVD main

--- Lesson4_2/Lesson4_2.cpp
+++ Lesson4_2/Lesson4_2.cpp
@@ -1,7 +1,12 @@
 #include "MusicCD.h"
+#include "MovieDVD.h"
+
 
 int main()
 {
-	MusicCD a("Clint Eastwood", "Gorillaz", 2001, 101, 4 * 60);
+	MusicCD a("Clint Eastwood", "Gorillaz", 2001, 101, 4);
 	a.showInfo();
+	cout << "\n--------------------------------------------------\n";
+	MovieDVD b("Silence of the lambs", "Jonathan Demme", 1991, 0.5, 2 * 60, "comedy", "USA", "Anthony Hopkins");
+	b.showInfo();
 }
